30895dac8a774b1f03f66bbb6afcd9394db3bd4b
Sam England <77686863+sam-eng11@users.noreply.github.com>
2021-03-21 06:11:04 -0400

    Add Emojipedia

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,13 @@
 import React from "react";
-import Card from "./Card";
-import contacts from "../contacts";
+import Entry from "./Entry";
+import emojipedia from "../emojipedia";
 
-function createCard(contact) {
+function createEmoji(emojipedia) {
   return (
-    <Card
-      id={contact.id}
-      key={contact.id}
-      name={contact.name}
-      img={contact.imgURL}
-      tel={contact.phone}
-      email={contact.email}
+    <Entry
+      emoji={emojipedia.emoji}
+      name={emojipedia.name}
+      description={emojipedia.meaning}
     />
   );
 }
@@ -18,26 +15,10 @@ function createCard(contact) {
 function App() {
   return (
     <div>
-      <h1 className="heading">My Contacts</h1>
-      {contacts.map(createCard)}
-      {/* <Card
-        name={contacts[0].name}
-        img={contacts[0].imgURL}
-        tel={contacts[0].phone}
-        email={contacts[0].email}
-      />
-      <Card
-        name={contacts[1].name}
-        img={contacts[1].imgURL}
-        tel={contacts[1].phone}
-        email={contacts[1].email}
-      />
-      <Card
-        name={contacts[2].name}
-        img={contacts[2].imgURL}
-        tel={contacts[2].phone}
-        email={contacts[2].email}
-      /> */}
+      <h1>
+        <span>emojipedia</span>
+      </h1>
+      <dl className="dictionary">{emojipedia.map(createEmoji)}</dl>
     </div>
   );
 }
