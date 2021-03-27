9d2e21cee2c27bb8855b416fda1c8d2120f0c6ac
Sam England <77686863+sam-eng11@users.noreply.github.com>
2021-03-27 06:01:50 -0400

    Keeper App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,38 +1,42 @@
 import React, { useState } from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import CreateArea from "./CreateArea";
 
 function App() {
-  const [inputText, setInputText] = useState("");
-  const [items, setItems] = useState([]);
+  const [notes, setNotes] = useState([]);
 
-  function handleChange(event) {
-    const inputItem = event.target.value;
-    setInputText(inputItem);
+  function addNote(newNote) {
+    setNotes(prevNotes => {
+      return [...prevNotes, newNote];
+    });
   }
 
-  function addItem() {
-    setItems((prevItems) => {
-      return [...prevItems, inputText];
+  function deleteNote(id) {
+    setNotes(prevNotes => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
     });
   }
 
   return (
-    <div className="container">
-      <div className="heading">
-        <h1>To-Do List</h1>
-      </div>
-      <div className="form">
-        <input onChange={handleChange} type="text" value={inputText} />
-        <button onClick={addItem}>
-          <span>Add</span>
-        </button>
-      </div>
-      <div>
-        <ul>
-          {items.map((todoItem) => {
-            return <li>{todoItem}</li>;
-          })}
-        </ul>
-      </div>
+    <div>
+      <Header />
+      <CreateArea onAdd={addNote} />
+      {notes.map((noteItem, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={noteItem.title}
+            content={noteItem.content}
+            onDelete={deleteNote}
+          />
+        );
+      })}
+      <Footer />
     </div>
   );
 }
